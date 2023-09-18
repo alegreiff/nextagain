d14c5c9e4c925061cf2ed91dbe5cc29f1133e84f
Jaime de Greiff <alegreiff@gmail.com>
2023-09-18 16:25:01 -0500

    Daisy

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoadingPage = () => {
+    return (
+        <>
+            <span className="loading loading-bars loading-lg"></span>
+
+        </>
+    )
+}
+
+export default LoadingPage
