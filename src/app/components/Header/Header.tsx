5416b8e3ea97de9b0132445e87be8250469b17a7
Jaime de Greiff <alegreiff@gmail.com>
2023-09-20 21:41:34 -0500

    menu limpio

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,11 +4,12 @@ import React from 'react'
 const HeaderNavigation = () => {
     return (
         <div className='bg-[#193d8a] p-4 text-2xl text-white font-bold flex justify-start'>
-            <Link className="btn btn-ghost p-1 m-1" href="/">Inicio</Link>
-            <Link className="btn btn-ghost p-1 m-1" href="/users/mapa">Mapa</Link>
-            <Link className="btn btn-ghost p-1 m-1" href="/users/leaf">Leaflet</Link>
+            <Link className="btn btn-ghost p-1 m-1" href="/">Datos generales</Link>
+            <Link className="btn btn-ghost p-1 m-1" href="/users/leaf">Origen visitas</Link>
+
+            {/* <Link className="btn btn-ghost p-1 m-1" href="/users/mapa">Mapa</Link>
             <Link className="btn btn-ghost p-1 m-1" href="/users">Usuarios</Link>
-            <Link className="btn btn-ghost p-1 m-1" href="/users/new">Demo</Link>
+            <Link className="btn btn-ghost p-1 m-1" href="/users/new">Demo</Link> */}
         </div>
     )
 }
